0df4a8f7f49673fbe7a259b5525396bf505f71f5
mohammed2950 <52863840+mohammed2950@users.noreply.github.com>
2020-06-21 17:51:59 -0400

    initial commit

---
+++ app/src/main/java/com/arifali/pizza_store/Pizza.java
@@ -0,0 +1,24 @@
+package com.arifali.pizza_store;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza implements Serializable {
+    List<String> toppings = new ArrayList<>();
+    boolean check;
+
+    public Pizza(List toppings, boolean check) {
+        this.toppings = toppings;
+        this.check = check;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "toppings=" + toppings +
+                ", check=" + check +
+                '}';
+    }
+}
